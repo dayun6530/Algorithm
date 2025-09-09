dee2eec8b236f6fe2e5c4e597c4c8441f43c347b
Dayun <dlekdbs6530@gmail.com>
2025-09-10 02:04:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int arr[101][101];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << '*' ;
+		}
+		cout << endl;
+	}
+}
