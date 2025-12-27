c165abbd5dc5793afee4e4d873308e654ab322d2
Dayun <dlekdbs6530@gmail.com>
2025-12-27 21:06:14 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+bool Check(string str) {
+	int len = (int)str.size(); 
+	stack<char> st;
+
+	for (int i = 0; i < len; i++) {
+		char c = str[i];
+
+		if (c == '(') {
+			st.push(str[i]);
+		}
+		else {
+			if (!st.empty()) {
+				st.pop();
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return st.empty();
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+
+		if (Check(str)) {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
