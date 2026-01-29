d477ef4ab709323c630582558514a834d3d65ab9
Dayun <dlekdbs6530@gmail.com>
2026-01-29 10:17:30 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int arr[1000][3];
+	int dp[1000][3];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	dp[0][0] = arr[0][0];
+	dp[0][1] = arr[0][1];
+	dp[0][2] = arr[0][2];
+
+	for (int i = 1; i < n; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+	}
+    
+    int ans = 1000000000;
+	for (int i = 0; i < 3; i++) {
+		ans = min(ans, dp[n-1][i]);
+	}
+
+	cout << ans;
+	return 0;
+}
