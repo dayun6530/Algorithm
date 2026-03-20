eb2739dc070b2770520758781ded72553b484ad6
Dayun <dlekdbs6530@gmail.com>
2026-03-20 22:40:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 1992 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int N;
+int arr[1001];
+int dp[1001];
+int maxNum = 0;
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < N; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) {
+				dp[i] = dp[i] + 1;
+			}
+		}
+	}
+	
+	for (int i = 0; i < N; i++) {
+		maxNum = max(maxNum, dp[i]);
+	}
+
+	cout << maxNum << endl;
+	return 0;
+
+}
