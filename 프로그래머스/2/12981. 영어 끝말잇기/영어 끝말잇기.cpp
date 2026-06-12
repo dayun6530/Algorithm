fb4160f6f626f188fa8b084cfe4875ca28a11401
Dayun <dlekdbs6530@gmail.com>
2026-06-13 03:27:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words){
+    vector<int> answer;
+        
+    int flag = 0;
+    int a = 0;
+    int b =0;
+    
+    for(int i = 1; i<words.size(); i++){
+        string p1 = words[i-1];
+        string p2 = words[i];
+        
+        for(int j = 0; j<i; j++){
+            if(words[j] == words[i]){
+                flag = 1;
+                break;
+            }
+        }
+        
+        int len_p1 = p1.size();
+        
+        if(p1[len_p1 - 1] != p2[0]){
+            flag = 1;
+        }
+        if(flag){
+            a = (i%n) + 1;
+            b = (i/n) + 1;
+            answer.push_back(a);
+            answer.push_back(b);
+            return answer;
+        }
+    }
+    answer.push_back(0);
+    answer.push_back(0);
+    
+    return answer;
+}
