ea87bbe1a1d8da0da759bba6734d169d900d854e
Dayun <dlekdbs6530@gmail.com>
2026-07-26 01:40:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    
+    while(num != 1){
+        if(num % 2 == 0){
+            num = num/2;
+            answer++;
+        }else{
+            num = num*3+1;
+            answer++;
+        }
+        if(num < 0 || answer > 500){
+            answer = -1;
+            break;
+        }
+    }
+    
+    
+    
+    return answer;
+}
